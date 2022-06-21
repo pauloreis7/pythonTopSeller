a96462b7af274f2c3408b80f123a74e8c31c198e
pauloreis7 <paulosilvadosreis2057@gmail.com>
2022-06-21 11:47:41 -0300

    feat: get winner seller

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-msg = 'Hello World!'
+import pandas as pd
 
-print(msg)
+months_list = ['january', 'february', 'march', 'april', 'may', 'june']
+
+
+for month in months_list:
+    sells_table = pd.read_excel(f'{month}.xlsx')
+
+    if (sells_table['Vendas'] > 55000).any():
+
+        winner_name = sells_table.loc[sells_table['Vendas']
+                                      > 55000, 'Vendedor'].values[0]
+        winner_sells_amount = sells_table.loc[sells_table['Vendas']
+                                              > 55000, 'Vendas'].values[0]
+
+        print(
+            f'At {month} the target has been done! Seller: {winner_name}, Sells amount: {winner_sells_amount}')
